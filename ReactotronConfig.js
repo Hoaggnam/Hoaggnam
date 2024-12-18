5af1f8810529a806116e84c9cb295790650ab7dc
Hoaggnam <hoaggnam11122001@gmail.com>
2024-12-18 10:56:34 +0700

    Add files via upload

---
+++ ReactotronConfig.js
@@ -0,0 +1,32 @@
+import Reactotron from 'reactotron-react-native';
+
+Reactotron.configure({
+  name: 'React Native ZYZJ',
+  host: 'localhost',
+})
+  .useReactNative({
+    networking: {
+      // optionally, you can turn it off with false.
+      ignoreUrls: /symbolicate/,
+    },
+    editor: false, // there are more options to editor
+    errors: {veto: stackFrame => false}, // or turn it off with false
+    overlay: false, // just turning off overlay
+  })
+  .connect();
+
+// swizzle the old one
+const yeOldeConsoleLog = console.log;
+
+// make a new one
+console.log = (...args) => {
+  // always call the old one, because React Native does magic swizzling too
+  yeOldeConsoleLog(...args);
+
+  // send this off to Reactotron.
+  Reactotron.display({
+    name: 'CONSOLE.LOG',
+    value: args,
+    preview: args.length > 1 ? JSON.stringify(args) : args[0],
+  });
+};
